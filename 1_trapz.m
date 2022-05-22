dd1e7f390362b68c866e6cae1c7e0c61c93eef9f
Sahil Kolambkar <68233297+Sahil-Kolambkar@users.noreply.github.com>
2022-05-22 22:40:18 +0530

    Create 1_trapz.m

---
+++ 1_trapz.m
@@ -0,0 +1,13 @@
+clc
+clear all
+close all
+t = -2:0.001:2;
+xct = (t>=-1)-(t>=1); %rectangular pulse
+plot(t,xct);
+figure;
+w = -8*pi:0.01:8*pi;
+for i =1:length(w)
+    xcw(i) = trapz(t,xct.*exp(-1i*w(i).*t));
+end
+plot(w,abs(xcw));
+grid on;
